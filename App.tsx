4ba36cffaacc313756adbb7b0f6c258df918f5ed
julia <caralhodasilva04@gmail.com>
2026-03-26 21:46:50 -0300

    implementei a navegacao

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParams } from './navigation/types';
 import { SongView } from './View/SongView';
+import { SongListView } from './navigation/SongListView';
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#121212' }}>
-        <SongView />
-      </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="AddSong">
+        <Stack.Screen name="AddSong" component={SongView} />
+        <Stack.Screen name="SongList" component={SongListView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }

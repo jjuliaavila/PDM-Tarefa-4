97629491077b0a79c47691e8149ac09663ea32cc
julia <caralhodasilva04@gmail.com>
2026-04-01 08:29:45 -0300

    substitui o stack navigator pelo tab navigator

--- App.tsx
+++ App.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RootStackParams } from './navigation/types';
-import { SongView } from './View/SongView';
-import { SongListView } from './navigation/SongListView';
-
-const Stack = createNativeStackNavigator<RootStackParams>();
+import { TabNavigator } from './navigation/TabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AddSong">
-        <Stack.Screen name="AddSong" component={SongView} />
-        <Stack.Screen name="SongList" component={SongListView} />
-      </Stack.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
